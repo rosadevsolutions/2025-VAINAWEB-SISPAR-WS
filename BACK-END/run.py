36cc868061d20b88ca55a3da73869844b36ca335
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-07 22:56:03 -0300

    run.py melhoria nos comentários

--- BACK-END/run.py
+++ BACK-END/run.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
 
 """
 BUGS CORRIGIDOS
-1) INSTALAÇÃO DE MÓDULOS - NÃO SENDO RECONHECIDAS PELO PYLANCE
-Ao tentar instalar ao flask direto por pip install o vscode instalava na lib da venv mas o pylance não habilitava o modulo para uso.
-SOLUÇÃO: Acessar o Python Interpreter e fazer o path até venv/Scripts/python para instalar o módulo. Dessa forma o vscode instala o modulo no python presente na venv.
+1) INSTALAÇÃO DE MÓDULOS E CLASS - NÃO SENDO RECONHECIDAS PELO PYLANCE
+Ao tentar instalar o flask através do terminal com o comando pip install flask, o modulo era instalado na lib da venv, mas o pylance não habilitava o modulo e suas classes para uso.
+SOLUÇÃO: Acessar o Python Interpreter, acessar o path para o python que consta nos Scripts da venv e assim fazer a instalação do módulo (venv/Scripts/python). Dessa forma o modulo é instalado entre as libs da venv e o import é habilita pra uso em qualquer arquivo.
 OBS: Esse caminho deve ser realizado para instalar qualquer módulo.
 
 2) PYLINT - MENSAGENS DE ERRO DOCSTRING
-O erro ocorre devido a uma questão de convensão do Python e o Docstring.
+O erro ocorre devido a uma questão de convenção entre o Python e o Docstring.
 Diversos vídeos sugerem desabilitar o Pylint, o que achava ruim já que ele nos sinaliza problemas no código.
 SOLUÇÃO: Incluir no Settings do usuario no VSCcode uma linha desabilitando somente as mensagens referentes aos erros de docstring
 """
