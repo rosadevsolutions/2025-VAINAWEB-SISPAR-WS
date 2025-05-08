532b84eb787b9ce2ad32fc696a1fb3fd8522dd2d
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-07 23:28:12 -0300

    run.py inclusao route colaboradores

--- BACK-END/run.py
+++ BACK-END/run.py
@@ -6,18 +6,33 @@ app = Flask(__name__)
 def hello():
     return 'teste main'
 
+@app.route('/colaboradores')
+def get_data_collaborators():
+    data_collaborators = {
+        'nome':'Romulo Rosa',
+        'cpf':'124.54.687-98',
+        'cargo':'Desenvolvedor FullStack'
+    }
+    return data_collaborators
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-"""
-BUGS CORRIGIDOS
-1) INSTALAÇÃO DE MÓDULOS E CLASS - NÃO SENDO RECONHECIDAS PELO PYLANCE
-Ao tentar instalar o flask através do terminal com o comando pip install flask, o modulo era instalado na lib da venv, mas o pylance não habilitava o modulo e suas classes para uso.
-SOLUÇÃO: Acessar o Python Interpreter, acessar o path para o python que consta nos Scripts da venv e assim fazer a instalação do módulo (venv/Scripts/python). Dessa forma o modulo é instalado entre as libs da venv e o import é habilita pra uso em qualquer arquivo.
-OBS: Esse caminho deve ser realizado para instalar qualquer módulo.
 
-2) PYLINT - MENSAGENS DE ERRO DOCSTRING
-O erro ocorre devido a uma questão de convenção entre o Python e o Docstring.
-Diversos vídeos sugerem desabilitar o Pylint, o que achava ruim já que ele nos sinaliza problemas no código.
-SOLUÇÃO: Incluir no Settings do usuario no VSCcode uma linha desabilitando somente as mensagens referentes aos erros de docstring
-"""
+#BUGS CORRIGIDOS
+# 1) INSTALAÇÃO DE MÓDULOS E CLASS - NÃO SENDO RECONHECIDAS PELO PYLANCE
+# Ao instalar o flask através do terminal com o comando pip install flask,
+# o modulo era instalado na lib da venv, mas o pylance não habilitava o modulo
+# e suas classes para uso.
+# SOLUÇÃO: Acessar o Python Interpreter, acessar o path para o python que
+# consta nos Scripts da venv e assim fazer a instalação do módulo
+# (venv/Scripts/python). Dessa forma o modulo é instalado entre as libs da venv
+# e o import é habilita pra uso em qualquer arquivo.
+# OBS: Esse caminho deve ser realizado para instalar qualquer módulo.
+
+# 2) PYLINT - MENSAGENS DE ERRO DOCSTRING
+# O erro ocorre devido a uma questão de convenção entre o Python e o Docstring.
+# Diversos vídeos sugerem desabilitar o Pylint, o que achava ruim já que ele
+# nos sinaliza problemas no código.
+# SOLUÇÃO: Incluir no Settings do usuario no VSCcode uma linha desabilitando
+# somente as mensagens referentes aos erros de docstring
