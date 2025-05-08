7a1b404c8c95159e861d6d8c3261d02b039ef59a
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-08 02:11:49 -0300

    bp_collaborator route troca pra dados

--- BACK-END/src/controller/colaborador_controller.py
+++ BACK-END/src/controller/colaborador_controller.py
@@ -2,9 +2,9 @@ from flask import Blueprint
 
 bp_collaborator = Blueprint('colaborador', __name__, url_prefix='/colaborador')
 
-@bp_collaborator.route('/colaboradores')
-def get_collaborators():
-    list_collaborators = [
+@bp_collaborator.route('/dados')
+def get_data_collaborators():
+    list_data_collaborators = [
         {'nome':'Wendel Santos', 'cargo':'Gerente Supply Chain', 'cracha':'000003'},
         {'nome':'Igor Maciel', 'cargo':'Tech Lead', 'cracha':'00105'},
         {'nome':'Rafael Borborema', 'cargo':'Gerente Financeiro', 'cracha':'000004'},
@@ -12,4 +12,4 @@ def get_collaborators():
         {'nome':'Lucas Machado', 'cargo':'Analista Financeiro', 'cracha':'000201'}
     ]
 
-    return list_collaborators
+    return list_data_collaborators
