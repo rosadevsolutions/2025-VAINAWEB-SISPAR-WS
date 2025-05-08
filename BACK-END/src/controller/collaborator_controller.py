8a5deeff8e5560427f9f962b9f751e54a69e2452
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-08 03:11:40 -0300

    collaborator_controller import request jsonify route cadastrar def create_collaborator

--- BACK-END/src/controller/collaborator_controller.py
+++ BACK-END/src/controller/collaborator_controller.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
-bp_collaborator = Blueprint('colaborador', __name__, url_prefix='/colaborador')
+bp_collaborator = Blueprint('colaboradores', __name__, url_prefix='/colaboradores')
 
-list_data_collaborators = [
+collaborator_list = [
     {'id':1, 'nome':'Wendel Santos', 'cargo':'Gerente Supply Chain', 'cracha':'000003'},
     {'id':2, 'nome':'Igor Maciel', 'cargo':'Tech Lead', 'cracha':'00105'},
     {'id':3, 'nome':'Rafael Borborema', 'cargo':'Gerente Financeiro', 'cracha':'000004'},
@@ -10,6 +10,22 @@ list_data_collaborators = [
     {'id':5, 'nome':'Lucas Machado', 'cargo':'Analista Financeiro', 'cracha':'000201'}
 ]
 
-@bp_collaborator.route('/dados')
-def get_data_collaborators():
-    return list_data_collaborators
+
+@bp_collaborator.route('/exibir')
+def read_collaborator():
+    return collaborator_list
+
+
+@bp_collaborator.route('/cadastrar', methods=['POST'])
+def create_collaborator():
+    collaborator_request = request.get_json()
+    collaborator_new = {
+        'id': len(collaborator_list) + 1,
+        'nome': collaborator_request['nome'],
+        'cargo': collaborator_request['cargo'],
+        'cracha': collaborator_request['cracha']
+    }
+
+    collaborator_list.append(collaborator_new)
+
+    return jsonify({'mensagem':'Colaborador cadastrado com sucesso!'}), 201
