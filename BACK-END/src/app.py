963391a3a4c57fe64d376d9cfcf6a3df307ab76f
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-08 02:02:17 -0300

    appy.py import bp_collaborator

--- BACK-END/src/app.py
+++ BACK-END/src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from src.controller.colaborador_controller import bp_collaborator
 
 def create_app():
-  app = Flask(__name__)
-  return app
+    app = Flask(__name__)
+    app.register_blueprint(bp_collaborator)
+    return app
