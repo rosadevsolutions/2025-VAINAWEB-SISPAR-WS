598eb1704639611f15b1d0ed884d01946ddd2896
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-08 02:28:36 -0300

    collaborator_controller list_data_collaborators escopo global e dicts com id

--- BACK-END/src/controller/collaborator_controller.py
+++ BACK-END/src/controller/collaborator_controller.py
@@ -2,14 +2,14 @@ from flask import Blueprint
 
 bp_collaborator = Blueprint('colaborador', __name__, url_prefix='/colaborador')
 
+list_data_collaborators = [
+    {'id':1, 'nome':'Wendel Santos', 'cargo':'Gerente Supply Chain', 'cracha':'000003'},
+    {'id':2, 'nome':'Igor Maciel', 'cargo':'Tech Lead', 'cracha':'00105'},
+    {'id':3, 'nome':'Rafael Borborema', 'cargo':'Gerente Financeiro', 'cracha':'000004'},
+    {'id':4, 'nome':'Elaine Veloso', 'cargo':'Coordenador Financeiro', 'cracha':'000102'},
+    {'id':5, 'nome':'Lucas Machado', 'cargo':'Analista Financeiro', 'cracha':'000201'}
+]
+
 @bp_collaborator.route('/dados')
 def get_data_collaborators():
-    list_data_collaborators = [
-        {'nome':'Wendel Santos', 'cargo':'Gerente Supply Chain', 'cracha':'000003'},
-        {'nome':'Igor Maciel', 'cargo':'Tech Lead', 'cracha':'00105'},
-        {'nome':'Rafael Borborema', 'cargo':'Gerente Financeiro', 'cracha':'000004'},
-        {'nome':'Elaine Veloso', 'cargo':'Coordenador Financeiro', 'cracha':'000102'},
-        {'nome':'Lucas Machado', 'cargo':'Analista Financeiro', 'cracha':'000201'}
-    ]
-
     return list_data_collaborators
