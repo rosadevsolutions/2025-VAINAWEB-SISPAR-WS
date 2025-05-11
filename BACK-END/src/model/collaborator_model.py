1db89670a4dc2c9a0fa9a48049c5ff7dffe8a8c5
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-11 19:15:50 -0300

    model collaborator_model trazer recursos pra criar tabelas e abstrair objetos

---
+++ BACK-END/src/model/collaborator_model.py
@@ -0,0 +1,20 @@
+from sqlalchemy.schema import Column
+from sqlalchemy.types import String, DECIMAL, Integer
+from src.model import db
+
+class Collaborator(db.Model):
+    __tablename__ = 'tb_collaborator'
+
+    id      = Column(Integer, primary_key=True, autoincrement=True)
+    nome    = Column(String(100))
+    email   = Column(String(100))
+    senha   = Column(String(100))
+    cargo   = Column(String(100))
+    salario = Column(DECIMAL)
+
+    def __init__ (self, nome, email, senha, cargo, salario):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.cargo = cargo
+        self.salario = salario
