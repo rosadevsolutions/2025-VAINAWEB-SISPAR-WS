847450b260692502ebe77bf7f09d92be1dbd14f1
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-11 18:56:58 -0300

    config.py variaveis de ambiente e conexao com bd

---
+++ BACK-END/config.py
@@ -0,0 +1,8 @@
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config():
+    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_DEV')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
