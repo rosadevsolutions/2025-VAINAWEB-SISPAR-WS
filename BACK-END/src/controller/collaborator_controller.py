45eb00c91e96bd0a782eda81373e07a8b157539b
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-12 02:10:58 -0300

    collaborator_controller insert new collaborator no db

--- BACK-END/src/controller/collaborator_controller.py
+++ BACK-END/src/controller/collaborator_controller.py
@@ -5,32 +5,70 @@ from src.model import db
 bp_collaborator = Blueprint('colaboradores', __name__, url_prefix='/colaboradores')
 
 collaborator_list = [
-    {'id':1, 'nome':'Wendel Santos', 'cargo':'Gerente Supply Chain', 'cracha':'000003'},
-    {'id':2, 'nome':'Igor Maciel', 'cargo':'Tech Lead', 'cracha':'00105'},
-    {'id':3, 'nome':'Rafael Borborema', 'cargo':'Gerente Financeiro', 'cracha':'000004'},
-    {'id':4, 'nome':'Elaine Veloso', 'cargo':'Coordenador Financeiro', 'cracha':'000102'},
-    {'id':5, 'nome':'Lucas Machado', 'cargo':'Analista Financeiro', 'cracha':'000201'}
+    {
+        'id':1,
+        'nome':'Wendel Santos',
+        'email': 'wendel@wss.com',
+        'senha':'1010',
+        'cargo':'Gerente Supply Chain',
+        'salario':'200000'
+    },
+    {
+        'id':2,
+        'nome':'Igor Maciel',
+        'email': 'igor@wss.com',
+        'senha':'2020',
+        'cargo':'Tech Lead',
+        'salario':'170000'
+    },
+    {
+        'id':3,
+        'nome':'Rafael Borborema',
+        'email': 'rafael@wss.com',
+        'senha':'3030',
+        'cargo':'Gerente Financeiro',
+        'salario':'200000'
+    },
+    {
+        'id':4,
+        'nome':'Elaine Veloso',
+        'email': 'elaine@wss.com',
+        'senha':'4040',
+        'cargo':'Coordenador Financeiro',
+        'salario':'170000'
+    },
+    {
+        'id':5,
+        'nome':'Lucas Machado',
+        'email': 'lucas@wss.com',
+        'senha':'5050',
+        'cargo':'Analista Financeiro',
+        'salario':'120000'
+    }
 ]
 
 
-@bp_collaborator.route('/exibir')
+@bp_collaborator.route('/exibir', methods=['GET'])
 def read_collaborator():
-    return collaborator_list
+    return jsonify(collaborator_list),200
 
 
 @bp_collaborator.route('/cadastrar', methods=['POST'])
 def create_collaborator():
     collaborator_request = request.get_json()
 
-    new_collaborator = Collaborator(
-        nome    = collaborator_request['nome'],
-        email   = collaborator_request['email'],
-        senha   = collaborator_request['senha'],
-        cargo   = collaborator_request['cargo'],
+    if not collaborator_request:
+        return jsonify( {'Erro': 'Todos os campos devem ser preenchidos'}), 400
+
+    collaborator_new = Collaborator(
+        nome = collaborator_request['nome'],
+        email = collaborator_request['email'],
+        senha = collaborator_request['senha'],
+        cargo = collaborator_request['cargo'],
         salario = collaborator_request['salario']
     )
 
-    db.session.add(new_collaborator)
+    db.session.add(collaborator_new)
     db.session.commit()
     return jsonify({'mensagem':'Colaborador cadastrado com sucesso!'}), 201
 
@@ -47,10 +85,16 @@ def update_collaborator(collaborator_id):
     if 'nome' in collaborator_request:
         collaborator_found['nome'] = collaborator_request['nome']
 
+    if 'email' in collaborator_request:
+        collaborator_found['email'] = collaborator_request['email']
+
+    if 'senha' in collaborator_request:
+        collaborator_found['senha'] = collaborator_request['senha']
+
     if 'cargo' in collaborator_request:
         collaborator_found['cargo'] = collaborator_request['cargo']
 
-    if 'cracha' in collaborator_request:
-        collaborator_found['cracha'] = collaborator_request['cracha']
+    if 'salario' in collaborator_request:
+        collaborator_found['salario'] = collaborator_request['salario']
 
     return jsonify({'mensagem':'Colaborador atualizado com sucesso!'}), 200
