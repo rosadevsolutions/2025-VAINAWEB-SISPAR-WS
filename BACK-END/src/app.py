b463903806bae6fc060a7f2f5d9f4b550c563255
Romulo Rosa | Rosadev Solutions <rosadevsolutions@gmail.com>
2025-05-11 22:19:23 -0300

    src app.py conexao db, verificacao contexto app pra criar entidades

--- BACK-END/src/app.py
+++ BACK-END/src/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from src.controller.collaborator_controller import bp_collaborator
+from src.model import db
+from config import Config
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp_collaborator)
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
     return app
